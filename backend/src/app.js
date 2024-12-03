03e699fc26f3a878dc5ace23c6b1f5adb2fe43d6
anshumancodes <anshumanprof01@gmail.com>
2024-12-04 05:07:30 +0530

    add express app instance file

---
+++ backend/src/app.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import bodyParser from 'body-parser';
+const app = express();
+app.use(cors());
+app.use(express.json({limit:"12kb"}));
+app.use(express.urlencoded({extended:true,limit:"12kb"}));
+app.use(express.static("public"));
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// routes for user management
+import userRoutes from './routes/user.routes.js';
+import productRoutes from './routes/product.routes.js';
+app.use('/api/user', userRoutes);
+app.use('/api/product', productRoutes);
+
+
+export default app;
