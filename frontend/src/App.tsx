a6d000e5080c8db84fdecae7e5b70a21d10c865e
anshumancodes <anshumanprof01@gmail.com>
2024-12-04 16:23:08 +0530

    remove React import that is not being used

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { lazy, Suspense } from "react";
+import  { lazy, Suspense } from "react";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Hero from "./components/Hero";
 import { useUserContext } from "./context/UserContext";
